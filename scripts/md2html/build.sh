d8f1f1b53de6e331dde308582903d9f24261bde7
Mike Ralphson <mike.ralphson@gmail.com>
2018-06-14 12:18:32 +0100

    Deploy html version of OAS

---
+++ scripts/md2html/build.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir -p deploy/oas
+mkdir -p deploy/js
+cd scripts/md2html
+cp -p js/* ../../deploy/js
+cp -p markdown/* ../../deploy/
+
+node md2html.js --respec --maintainers ./history/MAINTAINERS_v2.0.md ../../versions/2.0.md > ../../deploy/oas/v2.0.html
+
+for filename in ../../versions/3.*.md ; do
+  node md2html.js --respec --maintainers ../../MAINTAINERS.md ${filename} > ../../deploy/oas/v$(basename "$filename" .md).html
+done
+
---
+++ scripts/md2html/markdown/index.md
@@ -0,0 +1,7 @@
+## Current version
+
+* [v3.0.0](oas/v3.0.0.html)
+
+## Previous version
+
+* [v2.0](oas/v2.0.html)
